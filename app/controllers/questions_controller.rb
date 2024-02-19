634ceca0af0b96e3548c8109be8b74c3ecb45429
sema karan <karan18sema@gmail.com>
2024-02-19 14:29:40 +0100

    add rails functionality

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    @question = params[:question] if params[:question]
+    @question
+  end
+
+  def answer
+    @question = ask
+    if @question == "I am going to work".downcase
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @answer
+  end
+end
